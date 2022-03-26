d84c1a6efd3ae2f88f28c2a377aea794d8dd8bca
Drew C <jayedeem@gmail.com>
2022-03-26 11:01:05 -0700

    done

--- Python/1-Fundamentals/workshop2/app.py
+++ Python/1-Fundamentals/workshop2/app.py
@@ -1,10 +1,16 @@
-from banking_pkg.account import BankOptions
+'''This is ATM Module'''
+from banking_pkg.account import ATMOptions
+
+banking_user = ATMOptions()
 
 
-def atm_menu(user):
+def atm_menu(name: str) -> None:
+    ''''
+    atm menu
+    '''
     print("")
     print("          === Automated Teller Machine ===          ")
-    print("User: " + user)
+    print("User: " + name)
     print("------------------------------------------")
     print("| 1.    Balance     | 2.    Deposit      |")
     print("------------------------------------------")
@@ -14,36 +20,37 @@ def atm_menu(user):
 
 
 print("          === Automated Teller Machine ===          ")
+banking_user.name = input("Enter your name: ")
+banking_user_pin = int(input("Enter Pin: "))
 
-name = input("Enter name to register: ")
-pin = input("Enter PIN: ")
-BALANCE = 0
-print(f"{name} has been registerd with a starting balance of ${float(BALANCE)}")
+print(f"{banking_user.name} has been registered with a starting balance of ${banking_user.show_balance()}")
 print("Login")
-
-current_user = BankOptions(BALANCE, name)
 
 while True:
     name_to_validate = input("Enter name: ")
-    pin_to_validate = input("Enter PIN: ")
-    if name_to_validate != name and pin_to_validate != pin:
+    pin_to_validate = int(input("Enter Pin: "))
+    if name_to_validate != banking_user.name and pin_to_validate != banking_user_pin:
         print("Invalid")
     else:
         print("Success")
         break
 
 while True:
-    atm_menu(name)
-    print(f"Current Balance: {current_user.show_balance()}")
+    atm_menu(banking_user.name)
+    banking_user.show_balance()
     option = input("Choose an option: ")
+
     if option in ("1"):
-        print(f"Current Balance: {current_user.show_balance()}")
+        print(f"Current balance ${banking_user.show_balance()}")
+
     if option in ("2"):
         amount = float(input("Amount to deposit: "))
-        current_user.deposit(amount)
+        banking_user.deposit(amount)
+
     if option in ("3"):
-        amount = float(input("Amount to widthdraw: "))
-        current_user.widthdraw(amount)
+        amount = float(input("Amount to withdraw: "))
+        banking_user.withdraw(amount)
+
     if option in ("4"):
-        current_user.logout()
+        banking_user.logout()
         break
--- Python/1-Fundamentals/workshop2/banking_pkg/account.py
+++ Python/1-Fundamentals/workshop2/banking_pkg/account.py
@@ -1,17 +1,41 @@
-class BankOptions:
+'''This is ATM Module'''
 
-    def __init__(self, balance=0, name=""):
+
+class ATMOptions:
+    '''
+    ATM Options
+    '''
+    # init construct
+
+    def __init__(self, balance=0.0, name="") -> None:
         self.balance = balance
-        self.name = str(name)
+        self.name = name
 
-    def show_balance(self):
+    def show_balance(self) -> float:
+        '''
+        show_balance
+        return balance
+        '''
+        print("${:,.2f}".format(self.balance))
         return self.balance
+# depost
 
-    def deposit(self, amount):
+    def deposit(self, amount) -> None:
+        '''
+        deposit
+        '''
         self.balance += amount
 
-    def widthdraw(self, amount):
-        self.balance -= float(amount)
+# widthdraw
+    def withdraw(self, amount) -> None:
+        '''
+        withdraw
+        '''
+        self.balance -= amount
 
-    def logout(self):
-        print(f"Goodbye! {self.name}")
+# logout
+    def logout(self) -> None:
+        '''
+        logout
+        '''
+        print(f"Goodbye {self.name}")
